acff55f68acab4460c83ac98ce58d4ef81766d4f
pamlib <82670434+pamlib@users.noreply.github.com>
2022-03-08 01:30:03 -0500

    Update 286090429.lua

--- contents/CustomModules/286090429.lua
+++ contents/CustomModules/286090429.lua
@@ -1,7 +1,6 @@
 local GuiLibrary = shared.GuiLibrary
 local GetLib = function(str) return loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/3dbfeuh/v/main/contents/Lib/" .. tostring(str)))() end
 local runcode = function(func) func() end
-
 runcode(function()
     local Silent_Lib = GetLib("Silent/arsenal.lua")
     local SilentAim = {["Enabled"] = false} SilentAim = GuiLibrary["ObjectsThatCanBeSaved"]["CombatWindow"]["Api"].CreateOptionsButton({
@@ -12,12 +11,12 @@ runcode(function()
         ["HoverText"] = "Shoot at targets without directly\naiming at them."
     })
     local PartToFire = {["Value"] = "Head"} PartToFire = SilentAim.CreateDropdown({
-		["Name"] = "Body Part",
-		["List"] = {"Head", "Torso", "Random"},
-		["Function"] = function(callback)
+        ["Name"] = "Body Part",
+        ["List"] = {"Head", "Torso", "Random"},
+        ["Function"] = function(callback)
             Silent_Lib.Set("BodyPartToAim", callback)
         end,
-	})
+    })
     local TeamCheck = {["Enabled"] = false} TeamCheck = SilentAim.CreateToggle({
         ["Name"] = "Team Check",
         ["Function"] = function(callback)
@@ -26,7 +25,6 @@ runcode(function()
         ["Default"] = false
     })
 end)
-
 runcode(function()
     local Esp_Lib = GetLib("player_esp.lua")
     local ESP = {["Enabled"] = false} ESP = GuiLibrary["ObjectsThatCanBeSaved"]["RenderWindow"]["Api"].CreateOptionsButton({
