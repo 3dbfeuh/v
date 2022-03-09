bfcb7c505eede4fdc32d17d31dd5f48d50d7a933
pamlib <82670434+pamlib@users.noreply.github.com>
2022-03-09 00:54:04 -0500

    Update AnyGame.lua

--- contents/AnyGame.lua
+++ contents/AnyGame.lua
@@ -1,83 +1,8 @@
 local GuiLibrary = shared.GuiLibrary
-local customdir = shared.VapeCustomDirectory
-local players = game:GetService("Players")
-local textservice = game:GetService("TextService")
-local lplr = players.LocalPlayer
-local workspace = game:GetService("Workspace")
-local RunService = game:GetService("RunService")
-local lighting = game:GetService("Lighting")
-local cam = workspace.CurrentCamera
-local targetinfo = shared.VapeTargetInfo
-local uis = game:GetService("UserInputService")
-local localmouse = lplr:GetMouse()
-local requestfunc = syn and syn.request or http and http.request or http_request or fluxus and fluxus.request or getgenv().request or request
-local getasset = getsynasset or getcustomasset
-
-local RenderStepTable = {}
-local StepTable = {}
-
-local BindToRenderStep = function(name, num, func)
-	if RenderStepTable[name] == nil then
-		RenderStepTable[name] = RunService.RenderStepped:Connect(func)
-	end
-end
-local UnbindFromRenderStep = function(name)
-	if RenderStepTable[name] ~= nil then
-		RenderStepTable[name]:Disconnect()
-		RenderStepTable[name] = nil
-	end
-end
-
-local BindToStepped = function(name, num, func)
-	if StepTable[name] == nil then
-		StepTable[name] = RunService.Stepped:Connect(func)
-	end
-end
-local UnbindFromStepped = function(name)
-	if StepTable[name] ~= nil then
-		StepTable[name]:Disconnect()
-		StepTable[name] = nil
-	end
-end
-
-local function createwarning(title, text, delay)
-	pcall(function()
-		local frame = GuiLibrary["CreateNotification"](title, text, delay, "assets/WarningNotification.png")
-		frame.Frame.BackgroundColor3 = Color3.fromRGB(236, 129, 44)
-		frame.Frame.Frame.BackgroundColor3 = Color3.fromRGB(236, 129, 44)
-	end)
-end
-
-local function getcustomassetfunc(path)
-	if not isfile(path) then
-		spawn(function()
-			local textlabel = Instance.new("TextLabel")
-			textlabel.Size = UDim2.new(1, 0, 0, 36)
-			textlabel.Text = "Downloading " .. path
-			textlabel.BackgroundTransparency = 1
-			textlabel.TextStrokeTransparency = 0
-			textlabel.TextSize = 30
-			textlabel.Font = Enum.Font.SourceSans
-			textlabel.TextColor3 = Color3.new(1, 1, 1)
-			textlabel.Position = UDim2.new(0, 0, 0, -36)
-			textlabel.Parent = GuiLibrary["MainGui"]
-			repeat wait() until isfile(path)
-			textlabel:Remove()
-		end)
-		local req = requestfunc({
-			Url = "https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/" .. path:gsub(customdir .. "assets", "assets"),
-			Method = "GET"
-		})
-		writefile(path, req.Body)
-	end
-	return getasset(path) 
-end
-
+local GetLib = function(str) return loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/3dbfeuh/v/main/contents/Lib/" .. tostring(str)))() end
 local runcode = function(func) func() end
-
-local Esp_Lib = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/3dbfeuh/v/main/contents/Lib/player_esp.lua"))()
-
 runcode(function()
+    local Esp_Lib = GetLib("player_esp.lua")
     local ESP = {["Enabled"] = false} ESP = GuiLibrary["ObjectsThatCanBeSaved"]["RenderWindow"]["Api"].CreateOptionsButton({
         ["Name"] = "ESP",
         ["Function"] = function(callback)
