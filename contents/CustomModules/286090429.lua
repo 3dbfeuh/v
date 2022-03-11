a1cdbfd41e02943dfab9da0eb1af0caa8b2ce507
pamlib <82670434+pamlib@users.noreply.github.com>
2022-03-10 21:56:43 -0500

    Update 286090429.lua

--- contents/CustomModules/286090429.lua
+++ contents/CustomModules/286090429.lua
@@ -1,6 +1,20 @@
 local GuiLibrary = shared.GuiLibrary
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
 local GetLib = function(str) return loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/3dbfeuh/v/main/contents/Lib/" .. tostring(str)))() end
 local runcode = function(func) func() end
+local StepTable = {}
+local BindToStepped = function(name, func)
+	if StepTable[name] == nil then
+		StepTable[name] = game:GetService("RunService").Stepped:Connect(func)
+	end
+end
+local UnbindFromStepped = function(name)
+	if StepTable[name] then
+		StepTable[name]:Disconnect()
+		StepTable[name] = nil
+	end
+end
 runcode(function()
     local Silent_Lib = GetLib("Silent/Dynamic.lua")
     local SilentAim = {["Enabled"] = false} SilentAim = GuiLibrary["ObjectsThatCanBeSaved"]["CombatWindow"]["Api"].CreateOptionsButton({
@@ -23,6 +37,28 @@ runcode(function()
             Silent_Lib.Set("TeamCheck", callback)
         end,
         ["Default"] = false
+    })
+end)
+runcode(function()
+	local IsBlatant = function() return GuiLibrary["ObjectsThatCanBeSaved"]["Blatant ModeToggle"]["Api"]["Enabled"] end
+	local Speed = {["Enabled"] = false} Speed = GuiLibrary["ObjectsThatCanBeSaved"]["BlatantWindow"]["Api"].CreateOptionsButton({
+        ["Name"] = "Speed",
+        ["Function"] = function(callback)
+			if callback and not IsBlatant() then return Speed["ToggleButton"](false) end
+			if callback then
+				BindToStepped("Speed", function()
+					if LocalPlayer and LocalPlayer.Character and LocalPlayer.Character:FindFirstChildOfClass("Humanoid") then
+						local Humanoid = LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
+						if Humanoid.MoveDirection.Magnitude > 0 then
+							LocalPlayer.Character:TranslateBy(Humanoid.MoveDirection / 3)
+						end
+					end
+				end)
+			else
+				UnbindFromStepped("Speed")
+			end
+        end,
+        ["HoverText"] = "Increase your player speed."
     })
 end)
 runcode(function()
