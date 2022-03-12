f4b57a51fc2f8c903b9bec2044e63cf675fca125
pamlib <82670434+pamlib@users.noreply.github.com>
2022-03-12 17:09:35 -0500

    Update ron.lua

--- contents/Translations/ron.lua
+++ contents/Translations/ron.lua
@@ -1,4 +1,11 @@
 return {
+  ["Combat"] = "Luptă",
+  ["Blatant"] = "Flagrante",
+  ["Utility"] = "Utilitate",
+  ["World"] = "Lume",
+  ["Friends"] = "Prietenii",
+  ["Profiles"] = "Profiluri",  
   ["Tracers"] = "Marcatori",
-  ["Team Check"] = "Verificare Echipă"
+  ["Team Check"] = "Verificare Echipă",
+  ["Speed"] = "Viteză"
 }
